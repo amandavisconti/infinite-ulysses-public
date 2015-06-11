c917d9a5724d07f8b1753d2e154a42ab751027da
avisconti <amandavisconti@2551276.no-reply.drupal.org>
2015-06-11 11:49:19 -0400

    More custom modules and theme

---
+++ Drupal sites:all/themes/radix/kits/default/theme-settings.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @file
+ * Theme settings.
+ */
+
+/**
+ * Implements theme_settings().
+ */
+function {{machine_name}}_form_system_theme_settings_alter(&$form, &$form_state) {
+  // Ensure this include file is loaded when the form is rebuilt from the cache.
+  $form_state['build_info']['files']['form'] = drupal_get_path('theme', 'default') . '/theme-settings.php';
+
+  // Add theme settings here.
+  $form['{{machine_name}}_theme_settings'] = array(
+    '#title' => t('Theme Settings'),
+    '#type' => 'fieldset',
+  );
+
+  // Return the additional form widgets.
+  return $form;
+}
